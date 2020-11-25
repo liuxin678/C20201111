f7f248cab60e0d27c8fdeadcc47bce80a9d0340a
liuxin678 <74294727+liuxin678@users.noreply.github.com>
2020-11-25 09:51:14 +0800

    Create 112502.大学排名.c

---
+++ 112502.大学排名.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+	int mingci;
+	scanf("%d",&mingci);
+	printf("全国大学排名\n");
+	if(mingci==1)
+	{
+	printf("北京大学");
+	}
+	else if(mingci==2)
+	{
+	printf("清华大学");
+	}
+	else if(mingci==3)
+	{
+	printf("华中科技大学");
+	}
+	else if(mingci==4)
+	{
+	printf("天津大学");
+	}
+	else if(mingci==5)
+	{
+	printf("中山大学");
+	}
+	else if(mingci==6)
+	{
+	printf("西安交通大学");
+	}
+	else if(mingci==7)
+	{
+	printf("四川大学");
+	}
+	else if(mingci==8)
+	{
+	printf("吉林大学");
+	}
+	else if(mingci==9)
+	{
+	printf("同济大学");
+	}
+	else if(mingci==10)
+	{
+	printf("北京师范大学");
+	}
+
+}
